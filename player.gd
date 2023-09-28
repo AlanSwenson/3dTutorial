4c3bfc5e1a0d051a5a92ad19597a2f6bd0e497a4
AlanSwenson <alan@alanswenson.dev>
2023-09-28 14:05:59 -0500

    Add UI and Music

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody3D
 
+# Emitted when the player was hit by a mob.
+# Put this at the top of the script.
+signal hit
+
 # How fast the player moves in meters per second.
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
@@ -40,8 +44,7 @@ func _physics_process(delta):
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 
-	# Moving the Character
-	velocity = target_velocity
+
 	
 	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
@@ -64,6 +67,16 @@ func _physics_process(delta):
 				mob.squash()
 				target_velocity.y = bounce_impulse
 		
-		
-		
+	# Moving the Character
+	velocity = target_velocity
 	move_and_slide()
+
+
+# And this function at the bottom.
+func die():
+	hit.emit()
+	queue_free()
+
+
+func _on_mob_detector_body_entered(body):
+	die()
